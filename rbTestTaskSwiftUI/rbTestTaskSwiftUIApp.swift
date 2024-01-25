540b7898a79fa8b9a60be02e489c08fccd18bf4e
Evgeny <florans1807@mail.ru>
2024-01-25 12:38:00 +0300

    Initial Commit

---
+++ rbTestTaskSwiftUI/rbTestTaskSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  rbTestTaskSwiftUIApp.swift
+//  rbTestTaskSwiftUI
+//
+//  Created by Флоранс on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct rbTestTaskSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
